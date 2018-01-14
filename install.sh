d882d15333aa94069456158e41a8d9dee996401c
PNPtutorials <parvinderyadav95@gmail.com>
2018-01-14 17:10:24 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,6 @@ Comment=
 Exec=/usr/lib/vino/vino-server
 StartupNotify=false
 Terminal=false
-Hidden=false" > /root/.config/autostart/vino.desktop
+Hidden=false" > /home/pi/.config/autostart/vino.desktop
 
 sudo echo "VNC autorun on startup done"
